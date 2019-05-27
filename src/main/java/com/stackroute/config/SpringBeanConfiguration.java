3ecb7b50cc5e24f5259e85338f3af942c7fd0f70
prasanna <prasannakumari879913@gmail.com>
2019-05-27 16:57:32 +0530

    task 4 completed

--- src/main/java/com/stackroute/config/SpringBeanConfiguration.java
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -1,8 +1,13 @@
 package com.stackroute.config;
 
+import com.stackroute.awareinterface.ApplicationContextAwareDemo;
+import com.stackroute.awareinterface.BeanFactoryAwareDemo;
+import com.stackroute.awareinterface.BeanNameAwareDemo;
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -22,6 +27,26 @@ public class SpringBeanConfiguration {
     public Movie getMovie() {
         return new Movie();
     }
+    @Bean(name="applicationContextAwareDemo")
+    public ApplicationContextAwareDemo getApplicationContextAwareDemo() {
+        return new ApplicationContextAwareDemo();
+    }
+    @Bean(name= "BeanFactoryAwareDemo")
+    public BeanFactoryAwareDemo getBeanFactoryAwareDemo(){
+       return new BeanFactoryAwareDemo();
+    }
+
+    @Bean(name= "BeanNameAwareDemo")
+    public BeanNameAwareDemo getBeanNameAwareDemo(){
+        return new BeanNameAwareDemo();
+    }
+
+
+
+
+
+
+
 
 /*  @Bean(name = "movie1")
     public Movie getActor2() {
